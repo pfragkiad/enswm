5c40725d303ed6add248d38a0ecb710bb19881c9
pfragkiad <57967801+pfragkiad@users.noreply.github.com>
2022-03-06 20:57:11 +0200

    Add files via upload

--- process_times/main.c
+++ process_times/main.c
@@ -44,10 +44,51 @@ void exportBtNearDurations()
     fclose(hFile2);
 }
 
+void exportCpuDurations()
+{
+    char *filename = "cpu_times.dat";
+    char *durationsFilename = "cpu_times.txt";
+
+    // get number of records
+    struct stat stats;
+    if (stat(filename, &stats) == -1)
+    {
+        perror("Could not retrieve file stats.");
+        return EXIT_FAILURE;
+    }
+    printf("File size [bytes]: %ld\n", stats.st_size);
+
+    long recordsCount = (stats.st_size - sizeof(int)) / (sizeof(long));
+    printf("Number of records: %ld\n", recordsCount);
+
+    FILE *hFile = fopen(filename, "rb");
+    int magicNumber;
+    fread(&magicNumber, sizeof(int), 1, hFile); // 4 bytes
+    printf("Magic number: %04X\n", magicNumber);
+
+    long *times; // 16 bytes
+    times = malloc(recordsCount * sizeof(long));
+    // read all times
+    fread(times, sizeof(long), recordsCount, hFile);
+    fclose(hFile);
+
+    // now export stats!
+    FILE *hFile2 = fopen(durationsFilename, "w");
+    for (int i = 0; i < recordsCount; i++)
+    {
+        fprintf(hFile2, "%ld\r\n", times[i]);
+    }
+
+    fclose(hFile2);
+}
+
+
 
 int main(int argc, char **argv)
 {
-    exportBtNearDurations();
+    //exportBtNearDurations();
+
+    exportCpuDurations();
 
     return EXIT_SUCCESS;
 }
